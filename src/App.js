64f01c5c491b2fbc8bafeb1770a20addea7440fe
ramrami-12 <wislely66@gmail.com>
2022-06-27 17:20:56 +0900

    befor deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import styles from "./styles/main.module.css"
 import { MainNavLinks } from "./components/navLinks";
 import { Home } from "./pages/home";
 import { Intro } from "./pages/introduction";
 import GIT from "./pages/github";
 import JS from "./pages/javascript";
 import REACT from "./pages/react";
-import { NodeJS } from "./pages/javasDetail";
-import ScriptJS from "./pages/scriptjs";
+import { NodeJS, ScriptJS, ClassJS } from "./pages/javasDetail";
 
 function App() {
-  const mainStyle = {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#F2D7D9",
-    fontFamily: "Nanum Gothic",
-    overflowY: "scroll",
-  };
   const divStyle = {
     position: "relative",
     top: "10px",
@@ -29,9 +19,9 @@ function App() {
     borderRadius: "30px",
   };
   return (
-    <div style={mainStyle}>
+    <div className={styles.main}>
       <div style={divStyle}>
-        <h1>Index</h1>
+        <h1 style={{color: "#596c80"}}>Index</h1>
         <BrowserRouter>
           <MainNavLinks />
           <Routes>
@@ -41,7 +31,7 @@ function App() {
             <Route path="/js" element={<JS />} />
             <Route path="/js/node" element={<NodeJS />} />
             <Route path="/js/script" element={<ScriptJS />} />
-            <Route path="/js/class" element={<NodeJS />} />
+            <Route path="/js/class" element={<ClassJS />} />
             <Route path="/js/data" element={<NodeJS />} />
             <Route path="/js/levelup" element={<NodeJS />} />
             <Route path="/react" element={<REACT />} />
